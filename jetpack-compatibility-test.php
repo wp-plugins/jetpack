2a226e5e898d2447e16e09d2bb30e36a04d964a4
mdawaffe <mdawaffe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-03 00:32:05 +0000

    Jetpack Compat
    Add wp_generate_password() and self-request tests.
    
    
    git-svn-id: https://plugins.svn.wordpress.org/jetpack/branches/jetpack-compatibility-test@470423 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jetpack-compatibility-test.php
+++ jetpack-compatibility-test.php
@@ -4,7 +4,7 @@
  * Plugin Name: Jetpack Compatibility Test
  * Description: Tests your site's compatibily with Jetpack.
  * Plugin URI: http://jetpack.me/
- * Version: 1.0
+ * Version: 1.1
  * Author: Automattic
  * Author URI: http://automattic.com/
  * License: GPL2+
@@ -13,7 +13,7 @@
 class Jetpack_Compatibility_Test {
 	var $tests = array();
 
-	function &init() {
+	function init() {
 		static $instance = false;
 
 		if ( $instance ) {
@@ -35,9 +35,32 @@ class Jetpack_Compatibility_Test {
 
 	function admin_page_load() {
 		add_action( 'admin_head', array( $this, 'admin_head' ) );
+		$self_xml_rpc_url = site_url( 'xmlrpc.php' );
+		$self_xml_rpc_url = preg_replace( '/^http:/', 'https:', $self_xml_rpc_url );
 
+		$this->tests['wp_generate_password'] = $this->wp_generate_password();
 		$this->tests['http']  = wp_remote_get(  'http://jetpack.wordpress.com/jetpack.test/1/' );
 		$this->tests['https'] = wp_remote_get( 'https://jetpack.wordpress.com/jetpack.test/1/' );
+		if ( preg_match( '/^https:/', $self_xml_rpc_url ) ) {
+			$this->tests['self']      = wp_remote_get( preg_replace( '/^https:/', 'http:', $self_xml_rpc_url ) );
+			$this->tests['self-sec']  = wp_remote_get( $self_xml_rpc_url, array( 'sslverify' => true ) );
+		} else {
+			$this->tests['self']      = wp_remote_get( $self_xml_rpc_url );
+		}
+	}
+
+	function wp_generate_password() {
+		$lengths = array( 1, 5, 10, 16, 32, 32 );
+		foreach ( $lengths as $length ) {
+			$password = wp_generate_password( $length, false );
+			$r[] = sprintf( '%2d -> %2d:%s', $length, strlen( $password ), $password );
+		}
+		if ( class_exists( 'ReflectionFunction' ) && is_callable( 'ReflectionFunction', 'export' ) ) {
+			$r['ReflectionFunction'] = "\n" . ReflectionFunction::export( 'wp_generate_password', true );
+		} else {
+			$r['ReflectionFunction'] = null;
+		}
+		return $r;
 	}
 
 	function admin_head() {
@@ -74,11 +97,23 @@ jQuery( function( $ ) {
 	<h2>Jetpack Compatibility Test <a id="jetpack-compatibility-test-select-all" class="button" href="#">Select All</a></h2>
 
 	<div id="jetpack-compatibility-test-wrapper">
+<h3>TEST: <code>wp_generate_password()</code></h3>
+<?php $this->output( $this->tests['wp_generate_password'] ); ?>
+
 <h3>TEST: HTTP Connection</h3>
 <?php $this->output( $this->tests['http'] ); ?>
 
 <h3>TEST: HTTPS Connection</h3>
 <?php $this->output( $this->tests['https'] ); ?>
+
+<h3>TEST: Self Connection</h3>
+<?php $this->output( $this->tests['self'] ); ?>
+
+<?php if ( isset( $this->tests['self-sec'] ) ) : ?>
+<h3>TEST: Self Connection (HTTPS)</h3>
+<?php $this->output( $this->tests['self-sec'] ); ?>
+<?php endif; ?>
+
 </div>
 <?php
 	}
