d5e9c4d248f7bb4d3c39f4cd8bb5cf2727cf7ab7
jshreve <jshreve@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-03 15:46:40 +0000

    Jetpack: Publicize
    * Fixed fatal error for missing parentheses in the JETPACK__VERSION check
    * Updates the basic UI to allow for testing of all functions added to date
    * Adds calls to the new delete and globalization endpoints
    
    
    
    git-svn-id: https://plugins.svn.wordpress.org/jetpack/branches/publicize@607485 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/publicize/publicize.php
+++ modules/publicize/publicize.php
@@ -11,18 +11,20 @@ class Publicize {
 	}
 	
 	function admin_page_load() {
-		if ( isset( $_GET['action'] ) && isset( $_GET['service'] ) ) {
-			$service_name = $_GET['service'];
-			
-			$verification = Jetpack::create_nonce( 'publicize' );
-				
-			$stats_options = get_option( 'stats_options' );
-			$stats_id = isset( $stats_options['blog_id'] ) ? $stats_options['blog_id'] : null;
+		if ( isset( $_GET['action'] ) ) {
+		
+			if ( isset( $_GET['service'] ) )
+				$service_name = $_GET['service'];
 			
 			switch ( $_GET['action'] ) {
 				case 'request':
 					check_admin_referer( 'keyring-request', 'kr_nonce' );
 					check_admin_referer( "keyring-request-$service_name", 'nonce' );
+					
+					$verification = Jetpack::create_nonce( 'publicize' );
+				
+					$stats_options = get_option( 'stats_options' );
+					$stats_id = isset( $stats_options['blog_id'] ) ? $stats_options['blog_id'] : null;
 			
 					$user = wp_get_current_user();
 					$redirect = $this->api_url( $service_name, urlencode_deep( array(
@@ -40,6 +42,7 @@ class Publicize {
 					exit;
 				break;
 				
+				case 'refresh':
 				case 'completed':
 					// Jetpack blog requests Publicize Connections via new XML-RPC method
 					Jetpack::load_xml_rpc_client();
@@ -56,7 +59,12 @@ class Publicize {
 				break;
 				
 				case 'delete':
-					/*Jetpack::load_xml_rpc_client();
+					$id = $_GET['id'];
+					
+					check_admin_referer( 'keyring-request', 'kr_nonce' );
+					check_admin_referer( "keyring-request-$service_name", 'nonce' );
+					
+					Jetpack::load_xml_rpc_client();
 					$xml = new Jetpack_IXR_Client();
 					$xml->query( 'jetpack.deletePublicizeConnection', $id );
 					
@@ -65,10 +73,26 @@ class Publicize {
 					}
 					
 					$response = $xml->getResponse();
-					Jetpack::update_option( 'publicize_connections', $response );*/
+					Jetpack::update_option( 'publicize_connections', $response );
 				break;
 				
+				case 'globalize':
+				case 'unglobalize':
+					$id = $_GET['id'];
 				
+					check_admin_referer( 'keyring-request', 'kr_nonce' );
+					check_admin_referer( "keyring-request-$service_name", 'nonce' );
+					
+					Jetpack::load_xml_rpc_client();
+					$xml = new Jetpack_IXR_Client();
+					$xml->query( 'jetpack.globalizePublicizeConnection', $id, $_GET['action'] );
+					
+					if ( $xml->isError() ) {
+						// @todo error here..
+					}
+					
+					$response = $xml->getResponse();
+					Jetpack::update_option( 'publicize_connections', $response );
 			}
 		}
 	}
@@ -101,8 +125,31 @@ class Publicize {
 		), menu_page_url( 'sharing', false ) );
 	}
 	
-	function disconnect_url() {
-		
+	// just for development - should be removed before release
+	function refresh_url() {
+		return add_query_arg( array (
+			'action'   => 'refresh',
+		), menu_page_url( 'sharing', false ) );
+	}
+	
+	function disconnect_url( $service_name, $id ) {
+		return add_query_arg( array (
+			'action'   => 'delete',
+			'service'  => $service_name,
+			'id'       => $id,
+			'kr_nonce' => wp_create_nonce( 'keyring-request' ),
+			'nonce'    => wp_create_nonce( "keyring-request-$service_name" ),
+		), menu_page_url( 'sharing', false ) );
+	}
+	
+	function globalize_url( $service_name, $id, $action = 'globalize' ) {
+		return add_query_arg( array (
+			'action'   => $action,
+			'service'  => $service_name,
+			'id'       => $id,
+			'kr_nonce' => wp_create_nonce( 'keyring-request' ),
+			'nonce'    => wp_create_nonce( "keyring-request-$service_name" ),
+		), menu_page_url( 'sharing', false ) );
 	}
 	
 }
--- modules/publicize/ui.php
+++ modules/publicize/ui.php
@@ -18,14 +18,39 @@ class Publicize_UI {
 		<h3 id="publicize"><?php _e( 'Publicize', 'jetpack' ) ?></h3>
 		<p><?php _e( 'Connect your blog to popular social networking sites and automatically share new posts with your friends.', 'jetpack' ) ?></p>
 		
-		<a href="<?php echo $this->publicize->connect_url( 'twitter' ); ?>"><?php _e( 'Connect to Twitter', 'jetpack' ); ?></a>
+		<p>
+			<a href="<?php echo esc_url( $this->publicize->connect_url( 'twitter' ) ); ?>"><?php _e( 'Connect to Twitter', 'jetpack' ); ?></a> | 
+			<a href="<?php echo esc_url( $this->publicize->connect_url( 'facebook' ) ); ?>"><?php _e( 'Connect to Facebook', 'jetpack' ); ?></a> |
+			<a href="<?php echo esc_url( $this->publicize->connect_url( 'tumblr' ) ); ?>"><?php _e( 'Connect to Tumblr', 'jetpack' ); ?></a> | 
+			<a href="<?php echo esc_url( $this->publicize->connect_url( 'linkedin' ) ); ?>"><?php _e( 'Connect to LinkedIn', 'jetpack' ); ?></a> |
+			<a href="<?php echo esc_url( $this->publicize->connect_url( 'yahoo' ) ); ?>"><?php _e( 'Connect to Yahoo!', 'jetpack' ); ?></a>
+		</p>
+
+		<p><a href="<?php echo esc_url( $this->publicize->refresh_url() ); ?>">Refresh Connections</a></p>
 		
-		<pre>
-			<?php
-				$connections = Jetpack::get_option( 'publicize_connections' );
-				print_r($connections);
-			?>
-		</pre>
+		<?php
+			$all_connections = Jetpack::get_option( 'publicize_connections' );
+			
+			foreach( $all_connections as $service_name => $connections ) {
+				
+				echo "<h2>" . esc_html( $service_name ) . "</h2>";
+				foreach( $connections as $id => $connection ) {
+					?>
+						<p><strong><?php echo esc_html( $connection['external_display'] ); ?></strong>
+						<p><a href="<?php echo esc_url( $this->publicize->disconnect_url( $service_name, $id ) ); ?>">Disconnect</a></p>
+					<?php if ( $connection['connection_data']['user_id'] > 0 ) { ?>
+						<p><a href="<?php echo esc_url( $this->publicize->globalize_url( $service_name, $id, 'globalize' ) ); ?>">Globalize</a></p>
+					<?php } else { ?>
+						<p><a href="<?php echo esc_url( $this->publicize->globalize_url( $service_name, $id, 'unglobalize' ) ); ?>">Unglobalize</a></p>
+					<?php } 
+					
+					// there should be checks here that only allow a user to delete their own connection
+				}
+				
+			}
+			
+			echo "<pre>"; print_r($all_connections); echo "</pre>";
+		?>
 		
 		</form> <?php
 	}
