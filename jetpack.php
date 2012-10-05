3c9e7696cda4f194dc1f6028e063c5f06fc717e0
andy <andy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-05 19:00:27 +0000

    jetpack notes: remove debug link for non-admins
    git-svn-id: https://plugins.svn.wordpress.org/jetpack/branches/notes@608644 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jetpack.php
+++ jetpack.php
@@ -1996,7 +1996,9 @@ p {
 					<a href="http://jetpack.me/" target="_blank">Jetpack <?php echo esc_html( JETPACK__VERSION ); ?></a> |
 					<a href="http://automattic.com/privacy/" target="_blank"><?php _e( 'Privacy Policy', 'jetpack' ); ?></a> |
 					<a href="http://wordpress.com/tos/" target="_blank"><?php _e( 'Terms of Service', 'jetpack' ); ?></a> |
+<?php if ( current_user_can( 'manage_options' ) ) : ?>
 					<a href="<?php echo esc_url( wp_nonce_url( admin_url( 'admin-ajax.php?action=jetpack_debug' ), 'jetpack_debug' ) ); ?>" id="jp-debug"><?php _e( 'Debug', 'jetpack' ); ?></a> |
+<?php endif; ?>
 					<a href="http://jetpack.me/support/" target="_blank"><?php _e( 'Support', 'jetpack' ); ?></a>
 				</p>
 			</div>
