2cab81e2b43794aefaf746d56ea946f6ce1592d2
tmoorewp <tmoorewp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-05 15:32:09 +0000

    Publicize UI: Fix a warning if there are no connections.
    git-svn-id: https://plugins.svn.wordpress.org/jetpack/branches/publicize@608558 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/publicize/ui.php
+++ modules/publicize/ui.php
@@ -31,44 +31,46 @@ class Publicize_UI {
 		<?php
 			$all_connections = Jetpack::get_option( 'publicize_connections' );
 			
-			foreach( $all_connections as $service_name => $connections ) {
-				
-				echo "<h2>" . esc_html( $service_name ) . "</h2>";
-				foreach( $connections as $id => $connection ) {
-					?>
-						<p><strong><?php echo esc_html( $connection['external_display'] ); ?></strong>
-						<p><a href="<?php echo esc_url( $this->publicize->disconnect_url( $service_name, $id ) ); ?>">Disconnect</a></p>
-					<?php if ( $connection['connection_data']['user_id'] > 0 ) { ?>
-						<p><a href="<?php echo esc_url( $this->publicize->globalize_url( $service_name, $id, 'globalize' ) ); ?>">Globalize</a></p>
-					<?php } else { ?>
-						<p><a href="<?php echo esc_url( $this->publicize->globalize_url( $service_name, $id, 'unglobalize' ) ); ?>">Unglobalize</a></p>
-					<?php } 
+			if ( $all_connections ) {
+				foreach( $all_connections as $service_name => $connections ) {
 					
-					// the below is an example of how we would detect if we need an options popup
-					// see "temporary_options_example" in admin_page_load() for how the xml-rpc request works
-					if ( isset( $connection['connection_data']['meta']['options_responses'] ) ) {
-						$option_responses = $connection['connection_data']['meta']['options_responses'];
-						if ( is_array( $option_responses ) ) {
-							echo "Detected Options Cache - User Options Selection Should Show.";
-							
-							if ( 'tumblr' == $service_name ) {
-								$blogs = $option_responses[0]['response']['user']['blogs'];
-							
-								foreach ( $blogs as $blog ) {
-									if ( $blog['primary'] )
-										$blog_selected = parse_url( $blog['url'], PHP_URL_HOST );
-								} ?>
+					echo "<h2>" . esc_html( $service_name ) . "</h2>";
+					foreach( $connections as $id => $connection ) {
+						?>
+							<p><strong><?php echo esc_html( $connection['external_display'] ); ?></strong>
+							<p><a href="<?php echo esc_url( $this->publicize->disconnect_url( $service_name, $id ) ); ?>">Disconnect</a></p>
+						<?php if ( $connection['connection_data']['user_id'] > 0 ) { ?>
+							<p><a href="<?php echo esc_url( $this->publicize->globalize_url( $service_name, $id, 'globalize' ) ); ?>">Globalize</a></p>
+						<?php } else { ?>
+							<p><a href="<?php echo esc_url( $this->publicize->globalize_url( $service_name, $id, 'unglobalize' ) ); ?>">Unglobalize</a></p>
+						<?php } 
+						
+						// the below is an example of how we would detect if we need an options popup
+						// see "temporary_options_example" in admin_page_load() for how the xml-rpc request works
+						if ( isset( $connection['connection_data']['meta']['options_responses'] ) ) {
+							$option_responses = $connection['connection_data']['meta']['options_responses'];
+							if ( is_array( $option_responses ) ) {
+								echo "Detected Options Cache - User Options Selection Should Show.";
 								
-								<p><a href="<?php echo esc_url( $this->publicize->temporary_options_example_url( $service_name, $id, $blog_selected ) ); ?>">Example Options Set: Set Primary Blog</a></p>
-							
-								<?php
-							}		
-						}
-					}				
+								if ( 'tumblr' == $service_name ) {
+									$blogs = $option_responses[0]['response']['user']['blogs'];
+								
+									foreach ( $blogs as $blog ) {
+										if ( $blog['primary'] )
+											$blog_selected = parse_url( $blog['url'], PHP_URL_HOST );
+									} ?>
+									
+									<p><a href="<?php echo esc_url( $this->publicize->temporary_options_example_url( $service_name, $id, $blog_selected ) ); ?>">Example Options Set: Set Primary Blog</a></p>
+								
+									<?php
+								}		
+							}
+						}				
+						
+						// there should be checks here that only allow a user to delete their own connection
+					}
 					
-					// there should be checks here that only allow a user to delete their own connection
 				}
-				
 			}
 			
 			echo "<pre>"; print_r($all_connections); echo "</pre>";
