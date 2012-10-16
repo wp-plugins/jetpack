c96fc1b63fb5badcfd6f29aa09ba8bc8b5b3c149
beaulebens <beaulebens@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-16 14:31:17 +0000

    Strip trailing whitespace
    git-svn-id: https://plugins.svn.wordpress.org/jetpack/branches/publicize@613226 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/publicize/publicize-jetpack.php
+++ modules/publicize/publicize-jetpack.php
@@ -3,29 +3,29 @@ class Publicize extends Publicize_Base {
 
 	function __construct() {
 		parent::__construct();
-		
+
 		add_action( 'load-settings_page_sharing', array( $this, 'admin_page_load' ) );
-		
+
 		add_action( 'wp_ajax_publicize_tumblr_options_page', array( $this, 'options_page_tumblr' ) );
 		add_action( 'wp_ajax_publicize_facebook_options_page', array( $this, 'options_page_facebook' ) );
 		add_action( 'wp_ajax_publicize_twitter_options_page', array( $this, 'options_page_twitter' ) );
 		add_action( 'wp_ajax_publicize_linkedin_options_page', array( $this, 'options_page_linkedin' ) );
 		add_action( 'wp_ajax_publicize_yahoo_options_page', array( $this, 'options_page_yahoo' ) );
-		
+
 		add_action( 'wp_ajax_publicize_tumblr_options_save', array( $this, 'options_save_tumblr' ) );
 		add_action( 'wp_ajax_publicize_facebook_options_save', array( $this, 'options_save_facebook' ) );
 		add_action( 'wp_ajax_publicize_twitter_options_save', array( $this, 'options_save_twitter' ) );
 		add_action( 'wp_ajax_publicize_linkedin_options_save', array( $this, 'options_save_linkedin' ) );
-		add_action( 'wp_ajax_publicize_yahoo_options_save', array( $this, 'options_save_yahoo' ) );	
+		add_action( 'wp_ajax_publicize_yahoo_options_save', array( $this, 'options_save_yahoo' ) );
 	}
-	
+
 	// @todo only load current users conncetions and _user_id = 0
 	function get_connections( $service_name, $_blog_id = false, $_user_id = false ) {
 		$connections = Jetpack::get_option( 'publicize_connections' );
 		$connections_to_return = array();
-		
+
 		if ( !empty( $connections ) && is_array( $connections ) ) {
-			
+
 			if ( !empty( $connections[$service_name] ) ) {
 				foreach( $connections[$service_name] as $id => $connection ) {
 					if ( 0 == $connection['connection_data']['user_id'] || $this->user_id() == $connection['connection_data']['user_id'] ) {
@@ -37,32 +37,32 @@ class Publicize extends Publicize_Base {
 		}
 		return false;
 	}
-	
+
 	function get_connection_id( $connection ) {
 		return $connection['connection_data']['id'];
 	}
-	
+
 	function get_connection_meta( $connection ) {
 		$connection['connection_data'] =  (array) $connection['connection_data'];
 		return (array) $connection;
 	}
-	
+
 	function admin_page_load() {
 		if ( isset( $_GET['action'] ) ) {
-		
+
 			if ( isset( $_GET['service'] ) )
 				$service_name = $_GET['service'];
-			
+
 			switch ( $_GET['action'] ) {
 				case 'request':
 					check_admin_referer( 'keyring-request', 'kr_nonce' );
 					check_admin_referer( "keyring-request-$service_name", 'nonce' );
-					
+
 					$verification = Jetpack::create_nonce( 'publicize' );
-				
+
 					$stats_options = get_option( 'stats_options' );
 					$stats_id = isset( $stats_options['blog_id'] ) ? $stats_options['blog_id'] : null;
-			
+
 					$user = wp_get_current_user();
 					$redirect = $this->api_url( $service_name, urlencode_deep( array(
 						'action'       => 'request',
@@ -78,51 +78,51 @@ class Publicize extends Publicize_Base {
 					wp_redirect( $redirect );
 					exit;
 				break;
-				
+
 				case 'completed':
 					// Jetpack blog requests Publicize Connections via new XML-RPC method
 					Jetpack::load_xml_rpc_client();
 					$xml = new Jetpack_IXR_Client();
 					$xml->query( 'jetpack.fetchPublicizeConnections' );
-					
+
 					if ( !$xml->isError() ) {
 						$response = $xml->getResponse();
 						Jetpack::update_option( 'publicize_connections', $response );
 					}
 				break;
-				
+
 				case 'delete':
 					$id = $_GET['id'];
-					
+
 					check_admin_referer( 'keyring-request', 'kr_nonce' );
 					check_admin_referer( "keyring-request-$service_name", 'nonce' );
-					
+
 					Jetpack::load_xml_rpc_client();
 					$xml = new Jetpack_IXR_Client();
 					$xml->query( 'jetpack.deletePublicizeConnection', $id );
-					
+
 					if ( !$xml->isError() ) {
 						$response = $xml->getResponse();
 						Jetpack::update_option( 'publicize_connections', $response );
 					}
 				break;
 			}
-			
-			
+
+
 		}
 	}
-	
+
 	function globalization() {
 		if ( 'on' == $_REQUEST['global'] ) {
 			$id = $_POST['connection'];
-			
+
 			if ( !current_user_can( Publicize::GLOBAL_CAP ) )
 				return;
-				
+
 			Jetpack::load_xml_rpc_client();
 			$xml = new Jetpack_IXR_Client();
 			$xml->query( 'jetpack.globalizePublicizeConnection', $id, 'globalize' );
-		
+
 			if ( !$xml->isError() ) {
 				$response = $xml->getResponse();
 				Jetpack::update_option( 'publicize_connections', $response );
@@ -139,16 +139,16 @@ class Publicize extends Publicize_Base {
 	// on WordPress.com this is/calls Keyring::admin_url
 	function api_url( $service = false, $params = array() ) {
 		$url = apply_filters( 'publicize_api_url', 'https://public-api.wordpress.com/connect/?jetpack=publicize' );
-		
+
 		if ( $service )
 			$url = add_query_arg( array( 'service' => $service ), $url );
-		
+
 		if ( count ( $params ) )
 			$url = add_query_arg( $params, $url );
-			
+
 		return $url;
 	}
-	
+
 	function connect_url( $service_name ) {
 		return add_query_arg( array (
 			'action'   => 'request',
@@ -157,7 +157,7 @@ class Publicize extends Publicize_Base {
 			'nonce'    => wp_create_nonce( "keyring-request-$service_name" ),
 		), menu_page_url( 'sharing', false ) );
 	}
-	
+
 	function disconnect_url( $service_name, $id ) {
 		return add_query_arg( array (
 			'action'   => 'delete',
@@ -169,7 +169,7 @@ class Publicize extends Publicize_Base {
 	}
 
 	function get_services( $filter = 'all' ) {
-	
+
 		$services = array(
 				'facebook' => array(),
 				'twitter'  => array(),
@@ -177,9 +177,9 @@ class Publicize extends Publicize_Base {
 				'tumblr'   => array(),
 				'yahoo'    => array(),
 		);
-		
+
 		$connected_services = array();
-		
+
 		if ( 'all' == $filter ) {
 			return $services;
 		} else {
@@ -191,19 +191,19 @@ class Publicize extends Publicize_Base {
 			return $connected_services;
 		}
 	}
-	
+
 	function get_connection( $service, $id, $_blog_id, $_user_id ) {
 		// @todo implement
 	}
-	
+
 	function flag_post_for_publicize( $new_status, $old_status, $post ) {
 		// @todo implement
 	}
-	
+
 	function save_meta( $post_id, $post ) {
 		// @todo implement
 	}
-	
+
 	/**
 	* Options Code
 	*/
@@ -215,7 +215,7 @@ class Publicize extends Publicize_Base {
 
 		// Nonce check
 		check_admin_referer( 'options_page_facebook_' . $_REQUEST['connection'] );
-		
+
 		$me = $options_to_show[0];
 		$pages = $options_to_show[1]['data'];
 
@@ -241,11 +241,11 @@ class Publicize extends Publicize_Base {
 				//$this->store_token( null );
 			}
 		}
-		
+
 		?>
 
 		<div id="thickbox-content">
-		
+
 			<?php
 			ob_start();
 			Publicize_UI::connected_notice();
@@ -294,7 +294,7 @@ class Publicize extends Publicize_Base {
 				</table>
 
 			<?php endif; ?>
-			
+
 			<?php Publicize_UI::global_checkbox( 'facebook', $_REQUEST['connection'] ); ?>
 
 			<p style="text-align: center;">
@@ -342,12 +342,12 @@ class Publicize extends Publicize_Base {
 		Jetpack::load_xml_rpc_client();
 		$xml = new Jetpack_IXR_Client();
 		$xml->query( 'jetpack.setPublicizeOptions', $id, $options );
-		
+
 		if ( !$xml->isError() ) {
 			$response = $xml->getResponse();
 			Jetpack::update_option( 'publicize_connections', $response );
 		}
-		
+
 		$this->globalization();
 	}
 
@@ -359,9 +359,9 @@ class Publicize extends Publicize_Base {
 		$connection = $connected_services['tumblr'][$_POST['connection']];
 		$options_to_show = $connection['connection_data']['meta']['options_responses'];
 		$request = $options_to_show[0];
-		
+
 		$blogs = $request['response']['user']['blogs'];
-	
+
 		$blog_selected = false;
 
 		if ( !empty( $connection['connection_data']['meta']['tumblr_base_hostname'] ) ) {
@@ -383,7 +383,7 @@ class Publicize extends Publicize_Base {
 		} ?>
 
 		<div id="thickbox-content">
-		
+
 			<?php
 			ob_start();
 			Publicize_UI::connected_notice();
@@ -407,13 +407,13 @@ class Publicize extends Publicize_Base {
 			<?php } ?>
 
 			</ul>
-			
+
 			<?php Publicize_UI::global_checkbox( 'tumblr', $_REQUEST['connection'] ); ?>
 
 			<p style="text-align: center;">
 				<input type="submit" value="<?php esc_attr_e( 'Save these settings' ) ?>" class="button tumblr-options save-options" name="save" data-connection="<?php echo esc_attr( $_REQUEST['connection'] ); ?>" rel="<?php echo wp_create_nonce( 'save_tumblr_blog_' . $_REQUEST['connection'] ) ?>" />
 			</p> <br />
-		</div> 
+		</div>
 
 		<?php
 	}
@@ -427,37 +427,37 @@ class Publicize extends Publicize_Base {
 		check_admin_referer( 'save_tumblr_blog_' . $_REQUEST['connection'] );
 
 		$id = $_POST['connection'];
-		
+
 		$options = array( 'tumblr_base_hostname' => $_POST['selected_id'] );
-		
+
 		Jetpack::load_xml_rpc_client();
 		$xml = new Jetpack_IXR_Client();
 		$xml->query( 'jetpack.setPublicizeOptions', $id, $options );
-		
+
 		if ( !$xml->isError() ) {
 			$response = $xml->getResponse();
 			Jetpack::update_option( 'publicize_connections', $response );
 		}
-		
+
 		$this->globalization();
 	}
-	
+
 	function options_page_twitter() { Publicize_UI::options_page_other( 'twitter'); }
 	function options_page_linkedin() { Publicize_UI::options_page_other( 'linkedin'); }
 	function options_page_yahoo() { Publicize_UI::options_page_other( 'yahoo'); }
-		
+
 	function options_save_twitter() { $this->options_save_other( 'twitter'); }
 	function options_save_linkedin() { $this->options_save_other( 'linkedin'); }
 	function options_save_yahoo() { $this->options_save_other( 'yahoo'); }
 	function options_save_other( $service_name ) {
 		// Nonce check
 		check_admin_referer( 'save_' . $service_name . '_blog_' . $_REQUEST['connection'] );
-		$this->globalization();	
+		$this->globalization();
 	}
 
 	// stub
 	function refresh_tokens_message() {
-		
-	}	
+
+	}
 }
 ?>
