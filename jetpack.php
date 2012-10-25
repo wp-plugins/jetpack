be34985ceebde3e1c43901b021d76bf0c357c72e
pento <pento@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-25 05:01:52 +0000

    Create a skeleton module for Post by Email.
    git-svn-id: https://plugins.svn.wordpress.org/jetpack/branches/post-by-email@617163 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jetpack.php
+++ jetpack.php
@@ -667,6 +667,7 @@ class Jetpack {
 			case 'comments' :
 			case 'carousel' :
 			case 'minileven':
+			case 'post-by-email':
 				break;
 
 			// These modules are default off if we think the site is a private one
---
+++ modules/post-by-email.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Module Name: Post by Email
+ * Module Description: Publish posts to your blog directly from your personal email account.
+ * First Introduced: 1.9
+ * Sort Order: 4
+ */
+
+function jetpack_load_post_by_email() {
+}
+
+add_action( 'jetpack_modules_loaded', 'jetpack_load_post_by_email' );
