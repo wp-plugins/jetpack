7a6d0a609c58e4c69fb8cc422c0833a2a25ee70b
cfinke <cfinke@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-19 01:42:51 +0000

    Tiled Galleries: Linking to media settings screen from the Tiled Gallery config button.
    git-svn-id: https://plugins.svn.wordpress.org/jetpack/branches/tiled-galleries@641434 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/tiled-gallery.php
+++ modules/tiled-gallery.php
@@ -10,4 +10,16 @@ function jetpack_load_tiled_gallery() {
 	include dirname( __FILE__ ) . "/tiled-gallery/tiled-gallery.php";
 }
 
+add_action( 'jetpack_modules_loaded', 'jetpack_tiled_gallery_loaded' );
+
+function jetpack_tiled_gallery_loaded() {
+	Jetpack::enable_module_configurable( __FILE__ );
+	Jetpack::module_configuration_load( __FILE__, 'jetpack_tiled_gallery_configuration_load' );
+}
+
+function jetpack_tiled_gallery_configuration_load() {
+	wp_safe_redirect( admin_url( 'options-media.php' ) );
+	exit;
+}
+
 jetpack_load_tiled_gallery();
