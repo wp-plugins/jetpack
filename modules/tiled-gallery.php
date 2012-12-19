2ff6c98a780acf3d96b388683524aaa26c915b2f
cfinke <cfinke@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-19 00:51:49 +0000

    Tiled Galleries: Initial commit with basic functionality.
    git-svn-id: https://plugins.svn.wordpress.org/jetpack/branches/tiled-galleries@641418 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ modules/tiled-gallery.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Module Name: Tiled Galleries
+ * Module Description:
+ * First Introduced: 2.1
+ */
+
+function jetpack_load_tiled_gallery() {
+	include dirname( __FILE__ ) . "/tiled-gallery/tiled-gallery.php";
+}
+
+jetpack_load_tiled_gallery();
