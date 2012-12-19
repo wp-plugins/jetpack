ad72bc481f4a4643330da8dae441455fa01f2fbb
cfinke <cfinke@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-19 01:35:46 +0000

    Tiled Gallery: Extended description and screenshot.
    git-svn-id: https://plugins.svn.wordpress.org/jetpack/branches/tiled-galleries@641433 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/module-info.php
+++ modules/module-info.php
@@ -734,13 +734,18 @@ add_action( 'jetpack_learn_more_button_photon', 'jetpack_photon_more_link' );
 function jetpack_tiled_gallery_more_info() { ?>
 	<h4><?php esc_html_e( 'Tiled Galleries' , 'jetpack' ); ?></h4>
 
-	<p><?php esc_html_e( '', 'jetpack' ); ?></p>
+	<div class="jp-info-img">
+		<img class="jp-info-img" src="<?php echo plugins_url( basename( dirname( dirname( __FILE__ ) ) ) . '/_inc/images/screenshots/tiled-gallery.png' ) ?>" alt="<?php esc_attr_e( 'Tiled Galleries', 'jetpack' ) ?>" width="300" height="150" />
+	</div>
+
+	<p><?php esc_html_e( 'Create elegant magazine-style mosaic layouts for your photos without having to use an external graphic editor.', 'jetpack' ); ?></p>
+	<p><?php printf( __( 'When adding a gallery to your post, you now have the option to select a layout style for your images. We\'ve added support for Rectangular, Square, and Circular galleries. By default, galleries will continue to display using the standard thumbnail grid layout. To make the rectangular layout the default for all of your site\'s galleries, head over to <a href="%s">Settings &rarr; Media</a> and check the box next to "Display all your gallery pictures in a cool mosaic."', 'jetpack' ), admin_url( 'options-media.php' ) ); ?></p>
 <?php
 }
-add_action( 'jetpack_module_more_info_tiled_gallery', 'jetpack_tiled_gallery_more_info' );
+add_action( 'jetpack_module_more_info_tiled-gallery', 'jetpack_tiled_gallery_more_info' );
 
 function jetpack_tiled_gallery_more_link() {
 	echo '<a class="button-secondary more-info-link" href="#">' . __( 'Learn More', 'jetpack' ) . '</a>';
 }
-add_action( 'jetpack_learn_more_button_tiled_gallery', 'jetpack_tiled_gallery_more_link' );
+add_action( 'jetpack_learn_more_button_tiled-gallery', 'jetpack_tiled_gallery_more_link' );
 // Tiled Galleries: STOP
