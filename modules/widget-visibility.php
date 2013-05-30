1b77d27f0c6a13800002d24445a938d6c99c023b
cfinke <cfinke@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-30 18:15:43 +0000

    Widget Visibility: Choose which pages to show your widgets on.
    git-svn-id: https://plugins.svn.wordpress.org/jetpack/branches/widget-visibility@720510 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ modules/widget-visibility.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Module Name: Widget Visibility
+ * Module Description: Control what pages your widgets appear on.
+ * Sort Order: 10
+ * First Introduced: 2.7
+ * Requires Connection: No
+ */
+
+include dirname( __FILE__ ) . "/widget-visibility/widget-conditions.php";
